ed0c7d5189ca300ba2d5abc86c9912b5bb9df478
. <destinynwigwe087@gmail.com>
2025-10-20 20:49:37 +0100

    Just creating my branch

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
-require('./models')
+const sequelize = require('./database/database')
+const PORT = 3500
 const app = express()
 app.use(express.json())
-const PORT = 3500
+
+
+const database = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log(`Connection has been established successfully.`);
+    } catch (error){
+        console.error(`Unable to connect to the database:`, error);
+    }
+}
+ database();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port: ${PORT}`)
 })
