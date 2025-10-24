03e0a43719835524ee2a467f5df32fc835dab3ee
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-10-24 01:13:09 +0100

    rounding off user onboarding

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,8 @@
 const emailSender = require('../middleware/nodemailer')
 const { User } = require('../models')
 const bcrypt = require('bcryptjs')
-const signUpTemplate = require("../utils/emailTemplate")
+const { signUpTemplate, resendOtpTemplate, forgotPasswordTemplate } = require('../utils/emailTemplate')
+const jwt = require('jsonwebtoken')
 
 exports.signUp = async (req, res, next) => {
   try {
@@ -24,11 +25,11 @@ exports.signUp = async (req, res, next) => {
     const newUser = await User.create({
       firstName,
       lastName,
-      email,
+      email: email.toLowerCase(),
       phoneNumber,
       password: hashedPassword,
       otp: otp,
-      otpExpiredAt: Date.now() + 1000 * 60,
+      otpExpiredAt: Date.now() + 1000 * 60 * 5,
     })
 
     const emailOptions = {
@@ -41,8 +42,246 @@ exports.signUp = async (req, res, next) => {
 
     return res.status(201).json({
       message: 'User created successfully',
-      data: newUser,
+      data: {
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+      },
     })
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.verify = async (req, res, next) => {
+  try {
+    const { email, otp } = req.body
+
+    const checkUser = await User.findOne({ where: { email: email.toLowerCase() } })
+    if (!checkUser) {
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    if (Date.now() > checkUser.otpExpiredAt) {
+      return res.status(400).json({
+        message: 'Otp expired, Please request another otp',
+      })
+    }
+
+    if (otp !== checkUser.otp) {
+      return res.status(400).json({
+        message: 'Invalid OTP',
+      })
+    }
+
+    if (checkUser.isVerified) {
+      return res.status(400).json({
+        message: 'User already verified, Please proceed to login',
+      })
+    }
+
+    checkUser.isVerified = true
+    checkUser.otp = null
+    checkUser.otpExpiredAt = null
+
+    await checkUser.save()
+
+    return res.status(200).json({
+      message: 'User verified successfully',
+      data: {
+        id: checkUser.id,
+        email: checkUser.email,
+        isVerified: checkUser.isVerified,
+      },
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+exports.resendOtp = async (req, res, next) => {
+  const { email } = req.body
+  try {
+    const user = await User.findOne({ where: { email: email.toLowerCase() } })
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    if (user.isVerified) {
+      return res.status(400).json({
+        message: 'User already verified',
+      })
+    }
+
+    const newOtp = Math.round(Math.random() * 1e6)
+      .toString()
+      .padStart(6, '0')
+
+    user.otp = newOtp
+    user.otpExpiredAt = Date.now() + 1000 * 60 * 5
+
+    await user.save()
+
+    const emailOptions = {
+      email: user.email,
+      subject: 'Sign up successful',
+      html: resendOtpTemplate(newOtp, user.firstName),
+    }
+
+    await emailSender(emailOptions)
+
+    return res.status(200).json({
+      message: 'OTP resent successfully',
+      data: {
+        email: user.email,
+        otpSent: true,
+      },
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.findOne({
+      where: {
+        email: email.toLowerCase(),
+      },
+    })
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    if (!user.isVerified) {
+      return res.status(400).json({
+        message: 'User not verified, please verify your account',
+      })
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password)
+    if (!comparePassword) {
+      return res.status(400).json({
+        message: 'Incorrect password',
+      })
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email, role: user.role }, process.env.JWT_SECRET, {
+      expiresIn: '2hr',
+    })
+
+    return res.status(200).json({
+      message: 'Login successfull',
+      data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+      },
+      token: token,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const { email } = req.body
+
+    const user = await User.findOne({
+      where: {
+        email: email.toLowerCase(),
+      },
+    })
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '20m' })
+
+    const link = `${req.protocol}://${req.get('host')}/user/reset/password/${token}`
+
+    const emailOptions = {
+      email: user.email,
+      subject: 'Reset password',
+      html: forgotPasswordTemplate(link, user.firstName),
+    }
+
+    await emailSender(emailOptions)
+    return res.status(200).json({
+      message: 'forgot password request sent',
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+exports.resetPassword = async (req, res, next) => {
+  const { token } = req.params
+  const { newPassword, confirmPassword } = req.body
+  try {
+    if (!newPassword && !confirmPassword) {
+      return res.status(400).json({
+        message: 'please provide both passwords',
+      })
+    }
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({
+        message: 'Passwords do not match',
+      })
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await User.findOne({ where: { id: decoded.id } })
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(newPassword, salt)
+
+    user.password = hashedPassword
+
+    const tokenExpiry = Date.now() + 1000 * 60 * 5
+
+    if (Date.now() > tokenExpiry) {
+      return res.status(400).json({
+        message: 'Password expired, resend a password',
+      })
+    }
+
+    user.resetPasswordExpiredAt = tokenExpiry
+
+    await user.save()
+
+    return res.status(200).json({
+      message: 'Password reset successfully',
+    })
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(400).json({ message: 'Reset link has expired' })
+    }
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(400).json({ message: 'Invalid or malformed token' })
+    }
+    next(error)
+  }
+}
+exports.changePassword = async (req, res, next) => {
+  const { id } = req.user
+  const { oldPassword, newPassword, confirmPassword } = req.body
+  try {
+    const user = await User.findOne({ where: { id } })
   } catch (error) {
     next(error)
   }
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { signUp } = require('../controller/userController')
+const { signUp, verify, resendOtp, login, forgotPassword, resetPassword } = require('../controller/userController')
 
 const route = express.Router()
 
@@ -77,4 +77,325 @@ const route = express.Router()
  */
 route.post('/user', signUp)
 
+
+
+
+/**
+ * @swagger
+ * /api/v1/user/verify:
+ *   post:
+ *     summary: Verify user OTP
+ *     tags: [Users]
+ *     description: Verify the user's account using the OTP sent to their email.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - otp
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: mdigban@gmail.com
+ *               otp:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: User verified successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User verified successfully
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                     isVerified:
+ *                       type: boolean
+ */
+route.post("/user/verify", verify)
+
+
+
+
+/**
+ * @swagger
+ * /api/v1/user/resend-otp:
+ *   post:
+ *     summary: Resend OTP for user verification
+ *     tags: [Users]
+ *     description: Allows users to request a new OTP if the previous one expired or wasn't received.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: mdigban@gmail.com
+ *     responses:
+ *       200:
+ *         description: OTP resent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: New OTP sent successfully
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     email:
+ *                       type: string
+ *                       example: mdigban@gmail.com
+ *                     otpSent:
+ *                       type: boolean
+ *                       example: true
+ *       404:
+ *         description: User not found
+ *       400:
+ *         description: User already verified
+ */
+route.post("/user/resend-otp", resendOtp)
+
+
+
+
+
+
+
+
+/**
+ * @swagger
+ * /api/v1/user/login:
+ *   post:
+ *     summary: Log in a user
+ *     description: Authenticates a registered and verified user, returning a JWT token if successful.
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: mdigban@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: 12345
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Login successful
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       example: 83e3a1fe8d9ab7ff15abac23
+ *                     firstName:
+ *                       type: string
+ *                       example: Mije
+ *                     lastName:
+ *                       type: string
+ *                       example: Digban
+ *                     email:
+ *                       type: string
+ *                       example: mdigban@gmail.com
+ *                     role:
+ *                       type: string
+ *                       example: user
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *       400:
+ *         description: User not verified or incorrect password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User not verified, please verify your account
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User not found
+ *       500:
+ *         description: Internal server error
+ */
+
+route.post("/user/login", login)
+
+
+
+
+
+/**
+ * @swagger
+ * /api/v1/user/forgot-password:
+ *   post:
+ *     summary: Request password reset
+ *     description: Sends a password reset link to the user's registered email address if the account exists.
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: mdigban@gmail.com
+ *     responses:
+ *       200:
+ *         description: Forgot password request sent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: forgot password request sent
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User not found
+ *       500:
+ *         description: Internal server error
+ */
+
+route.post("/user/forgot-password", forgotPassword)
+
+
+
+
+/**
+ * @swagger
+ * /api/v1/user/reset/password/{token}:
+ *   post:
+ *     summary: Reset user password
+ *     description: This endpoint allows a user to reset their password using a valid token sent via email. The token is verified and, if valid, the password is updated.
+ *     tags:
+ *       - Authentication
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         description: JWT token received via email for password reset
+ *         schema:
+ *           type: string
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - newPassword
+ *               - confirmPassword
+ *             properties:
+ *               newPassword:
+ *                 type: string
+ *                 format: password
+ *                 example: StrongPassword123!
+ *               confirmPassword:
+ *                 type: string
+ *                 format: password
+ *                 example: StrongPassword123!
+ *     responses:
+ *       200:
+ *         description: Password reset successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Password reset successfully
+ *       400:
+ *         description: Invalid input, expired token, or mismatched passwords
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Passwords do not match
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User not found
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal Server Error
+ */
+
+route.post("/user/reset/password/:token", resetPassword)
 module.exports = route
